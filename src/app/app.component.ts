505f1dabf1eaa421d1de82584b66479cab889830
Juan Carlos Valdés <idevkingos@gmail.com>
2020-09-30 13:01:55 -0300

    preparcial - listo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Country } from './models/country.interface';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'preparcial';
+  countries: Country[] = [];
+  regionChanged(evt) {
+    this.countries = evt;
+  }
 }
